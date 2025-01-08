e3a3aaf7342e89c80ec3e9cb000e30aff669ecdb
AntWill <easy_snail@inbox.ru>
2025-01-08 15:24:08 +0700

    Пагинация

---
+++ UrbanDjango_19_5/task1/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+
+class Buyer(models.Model):
+    name = models.CharField(max_length=100)
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+    # добавление метода __str__ может улучшить читаемость объектов модели
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=100)
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+    size = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    description = models.TextField()
+    age_limited = models.BooleanField(default=False)
+    buyer = models.ManyToManyField(
+        'Buyer', related_name='games')  # Связь с моделью Buyer
+
+    def __str__(self):
+        return self.title
+
+
+class News(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
