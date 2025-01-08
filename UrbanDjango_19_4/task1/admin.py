5d4c90528512afc933a4f0dc6befd3b508f9f1e6
AntWill <easy_snail@inbox.ru>
2025-01-08 13:49:55 +0700

    admin

--- UrbanDjango_19_4/task1/admin.py
+++ UrbanDjango_19_4/task1/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Buyer, Game
 
-# Register your models here.
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    
+    list_display = ('name', 'balance', 'age')  # Поля, которые будут отображаться в списке объектов
+    search_fields = ('name',)  # Поля, по которым можно будет искать объекты
+    list_filter = ('balance', 'age')  # Поля, по которым можно будет фильтровать объекты
+    list_per_page = 30  # Ограничение количества записей до 30
+    readonly_fields = ('balance',)  # Поле, доступное только для чтения
+    
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cost', 'size')
+    search_fields = ('title',)
+    list_filter = ('size', 'cost')
+    list_per_page = 20 
